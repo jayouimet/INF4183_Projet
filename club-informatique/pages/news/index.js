0c0774d91b5944d7e32f7639727b5c4a1e4e3197
jayouimet <jayouimet@hotmail.com>
2022-12-15 13:49:07 -0500

    routing init

---
+++ club-informatique/pages/news/index.js
@@ -0,0 +1,9 @@
+import styles from '../../styles/Home.module.css'
+
+export default function News() {
+  return (
+    <div className={styles.container}>
+      News
+    </div>
+  )
+}
---
+++ club-informatique/pages/projects/index.js
@@ -0,0 +1,9 @@
+import styles from '../../styles/Home.module.css'
+
+export default function Projects() {
+  return (
+    <div className={styles.container}>
+      Projects
+    </div>
+  )
+}
