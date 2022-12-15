fe50ba6694c7486a7ec5aaa44916134ef57e46cf
jayouimet <jayouimet@hotmail.com>
2022-12-15 13:21:21 -0500

    init hasura

--- club-informatique/pages/_app.js
+++ club-informatique/pages/_app.js
@@ -1,7 +1,28 @@
+import { ApolloClient, ApolloProvider, InMemoryCache, HttpLink } from '@apollo/client';
+import { useState } from 'react';
 import '../styles/globals.css'
 
+const createApolloClient = (authToken) => {
+  return new ApolloClient({
+    link: new HttpLink({
+      uri: 'http://localhost:8080/v1/graphql',
+      
+      /*headers: {
+        Authorization: `Bearer ${authToken}`
+      }*/
+    }),
+    cache: new InMemoryCache(),
+  });
+};
+
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  // Anonymous, no token required
+  const [client] = useState(createApolloClient());
+  return (
+    <ApolloProvider client={client}>
+      <Component {...pageProps} />
+    </ApolloProvider>
+  );
 }
 
 export default MyApp
