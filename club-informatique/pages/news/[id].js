422cccb09abb347aa58407832e2f945c78dc06d0
jayouimet <jayouimet@hotmail.com>
2022-12-15 13:52:49 -0500

    dynamic id

--- club-informatique/pages/news/[id].js
+++ club-informatique/pages/news/[id].js
@@ -1,9 +1,13 @@
 import styles from '../../styles/Home.module.css'
+import { useRouter } from 'next/router'
 
 export default function News() {
-  return (
-    <div className={styles.container}>
-      News 1
-    </div>
-  )
+    const router = useRouter()
+    const { id } = router.query;
+
+    return (
+        <div className={styles.container}>
+        News {id}
+        </div>
+    )
 }
