734f8a6e1653e75a576139e7e1480d7fbb431588
jayouimet <jayouimet@hotmail.com>
2022-12-15 21:54:20 -0500

    banner title 65 vw

--- club-informatique/components/banner/banner_title.js
+++ club-informatique/components/banner/banner_title.js
@@ -4,14 +4,14 @@ import { Grid } from "@mui/material";
 export default function BannerTitle({title, ...props}) {
     return (
         <Grid
-        container
-        justify="flex-end"
-        alignItems="center"
-        id={styles.title_box}
-        {...props}
-    >
-        <h1 id={styles.banner_title}>{title}</h1>
-    </Grid>
+            container
+            justify="flex-end"
+            alignItems="center"
+            id={styles.title_box}
+            {...props}
+        >
+            <h1 id={styles.banner_title}>{title}</h1>
+        </Grid>
     );
 }
   
